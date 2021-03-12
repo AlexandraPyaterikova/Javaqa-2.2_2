f2149e03e9612a5285f470bf2dd43a404821bf4c
AlexandraPyaterikova <78533399+AlexandraPyaterikova@users.noreply.github.com>
2021-03-12 18:55:19 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         float bmi = service.calculate(1.62F, 64F);
-        System.out.println(bmi);
+        System.out.println("Индекс массы тела: " + bmi);
     }
 }
